a31dc0b0f15d9efb98ddf73888d672f59852fd38
John Nyingi <JNyingi@resolution.lan>
2021-08-18 16:42:49 +0300

    Added Unit Tests

---
+++ sigma.tests/LexerTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+using sigma;
+using System.Collections.Generic;
+
+namespace sigma.tests
+{
+    public class LexerTests
+    {
+        Lexer lexer = null;
+        [Fact]
+        public void TestBasicNumberStructure()
+        {
+            string l_exp = "NUMBER:5 MULTIPLY:* NUMBER:6 ";
+            lexer = new Lexer("5 * 6");
+            List<Token> tokens = lexer.Generate_Tokens();
+            string l_str = lexer.ToString();
+
+            Assert.NotNull(tokens);
+            Assert.Equal(l_exp, l_str);
+        }
+
+        [Fact]
+        public void TestExceptionThrown()
+        {
+            lexer = new Lexer("R");
+            Assert.Throws<NotSupportedException>(() => lexer.Generate_Tokens());
+        }
+    }
+}
