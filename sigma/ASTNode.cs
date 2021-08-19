88ef655330d71e0a062246ee1b6c43fc61e24fa7
John Nyingi <JNyingi@resolution.lan>
2021-08-20 00:31:37 +0300

    Added Subtraction/Minus operationr | Also modified ASTNode

--- sigma/ASTNode.cs
+++ sigma/ASTNode.cs
@@ -8,14 +8,10 @@ namespace sigma
     {
         public abstract decimal Eval();
     }
-    public class ASTNode: IASTNode
+    public class ASTNode
     {
         public dynamic Node { get; set; } // store any type of node
 
-        public override decimal Eval()
-        {
-            return Node.Eval();
-        }
     }
 
     public class ASTNumber: IASTNode
@@ -59,5 +55,27 @@ namespace sigma
             return LeftNode.Eval() + RigthNode.Eval();
         }
 
+    }
+
+    public class ASTMinus: IASTNode
+    {
+        public dynamic LeftNode;
+        public dynamic RigthNode;
+
+        public ASTMinus(dynamic leftNode, dynamic rightNode)
+        {
+            LeftNode = leftNode;
+            RigthNode = rightNode;
+        }
+
+        public override string ToString()
+        {
+            return LeftNode.ToString() + "- " + RigthNode.ToString();
+        }
+
+        public override decimal Eval()
+        {
+            return LeftNode.Eval() - RigthNode.Eval();
+        }
     }
 }
