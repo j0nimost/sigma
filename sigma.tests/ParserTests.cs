cdc58d4affafa648375733df026ba0330a7bf9d1
John Nyingi <JNyingi@resolution.lan>
2021-08-20 00:32:36 +0300

    Added Tests for Both Addition and Minus

---
+++ sigma.tests/ParserTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace sigma.tests
+{
+    public class ParserTests
+    {
+        Lexer lexer = null;
+        [Fact]
+        public void TestAddition()
+        {
+            lexer = new Lexer("5 + 6");
+            List<Token> tokens = lexer.Generate_Tokens();
+            Parser parser = new Parser(tokens);
+
+            ASTNode result = parser.expression();
+
+            Assert.NotNull(result);
+            decimal res = result.Node.Eval();
+            Assert.Equal(11, res);
+        }
+
+        [Fact]
+        public void TestSubtraction()
+        {
+            lexer = new Lexer("87-7");
+            List<Token> tokens = lexer.Generate_Tokens();
+            Parser parser = new Parser(tokens);
+
+            ASTNode result = parser.expression();
+
+            Assert.NotNull(result);
+            decimal res = result.Node.Eval();
+            Assert.Equal(80, res);
+        }
+
+        [Fact]
+        public void TestAdditionandSubtraction()
+        {
+            lexer = new Lexer("5 + 6 - 5");
+            List<Token> tokens = lexer.Generate_Tokens();
+            Parser parser = new Parser(tokens);
+
+            ASTNode result = parser.expression();
+
+            Assert.NotNull(result);
+            decimal res = result.Node.Eval();
+            Assert.Equal(6, res);
+        }
+    }
+}
