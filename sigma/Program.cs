ce2d2a4e053850969263932855b11cd5d6a5ea24
John Nyingi <JNyingi@resolution.lan>
2021-08-25 15:07:09 +0300

    Added Chceker for Variables passed

--- sigma/Program.cs
+++ sigma/Program.cs
@@ -9,19 +9,31 @@ namespace sigma
         {
             Lexer lexer = null;
             Console.Write("Sigma > ");
+            int counter = 0;
             while (true)
             {
-                
+                if(counter > 0)
+                {
+                    Console.Write("\nSigma > ");
+                }
                 string text = Console.ReadLine();
-                
+                counter++;
+                // check if text is a variable
+                AST assignment = null;
+                if(Parser.LocalAssignment.TryGetValue(text, out assignment))
+                {
+                    Console.Write(assignment.Eval());
+                    continue;
+                }
                 
                 try
                 {
                     lexer = new Lexer(text);
                     List<Token> tokens = lexer.Generate_Tokens();
+                    //Console.WriteLine(lexer.ToString());
                     Parser parser = new Parser(tokens);
                     AST ast = parser.expression();
-                    Console.WriteLine(ast.ToString());
+                    //Console.WriteLine(ast.ToString());
                     Console.Write(ast.Eval());
                 }
                 catch (Exception ex)
@@ -33,7 +45,7 @@ namespace sigma
                 
                 //Console.WriteLine(lexer.ToString()); // only printing
 
-                Console.Write("\nSigma > ");
+                //Console.Write("\nSigma > ");
             }
         }
     }
