21fc62faea4334455d443a1da77073e9871932ac
John Nyingi <JNyingi@resolution.lan>
2021-08-25 11:42:38 +0300

    Added New Test for all Token Types

--- sigma.tests/LexerTests.cs
+++ sigma.tests/LexerTests.cs
@@ -26,5 +26,18 @@ namespace sigma.tests
             lexer = new Lexer("$");
             Assert.Throws<NotSupportedException>(() => lexer.Generate_Tokens());
         }
+
+        [Fact]
+        public void TestAllTypes()
+        {
+            string l_exp = "IDENTIFIER:YASSS NUMBER:5656 MULTIPLY:* NUMBER:454 DIVIDE:/ NUMBER:3434 IDENTIFIER:tuyt ";
+            lexer = new Lexer("YASSS 5656 * 454 / 3434 tuyt");
+            List<Token> tokens = lexer.Generate_Tokens();
+            string l_str = lexer.ToString();
+
+            Assert.NotNull(tokens);
+            Assert.Equal(l_exp, l_str);
+
+        }
     }
 }
