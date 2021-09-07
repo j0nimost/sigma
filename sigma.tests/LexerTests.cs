e20c430226fed657f5592882bc27fa5e1aaef331
John Nyingi <JNyingi@resolution.lan>
2021-09-07 22:48:58 +0300

    Added EOF Token

--- sigma.tests/LexerTests.cs
+++ sigma.tests/LexerTests.cs
@@ -11,7 +11,7 @@ namespace sigma.tests
         [Fact]
         public void TestBasicNumberStructure()
         {
-            string l_exp = "NUMBER:5 MULTIPLY:* NUMBER:6 ";
+            string l_exp = "NUMBER:5 MULTIPLY:* NUMBER:6 EOF: ";
             lexer = new Lexer("5 * 6");
             List<Token> tokens = lexer.Generate_Tokens();
             string l_str = lexer.ToString();
@@ -30,7 +30,7 @@ namespace sigma.tests
         [Fact]
         public void TestAllTypes()
         {
-            string l_exp = "IDENTIFIER:YASSS NUMBER:5656 MULTIPLY:* NUMBER:454 DIVIDE:/ NUMBER:3434 IDENTIFIER:tuyt ";
+            string l_exp = "IDENTIFIER:YASSS NUMBER:5656 MULTIPLY:* NUMBER:454 DIVIDE:/ NUMBER:3434 IDENTIFIER:tuyt EOF: ";
             lexer = new Lexer("YASSS 5656 * 454 / 3434 tuyt");
             List<Token> tokens = lexer.Generate_Tokens();
             string l_str = lexer.ToString();
@@ -43,7 +43,7 @@ namespace sigma.tests
         [Fact]
         public void TestStringTypes()
         {
-            string str_exp = "IDENTIFIER:ye EQ: STRING:DONDA ";
+            string str_exp = "IDENTIFIER:ye EQ: STRING:DONDA EOF: ";
             lexer = new Lexer("ye = \"DONDA\"");
             List<Token> tokens = lexer.Generate_Tokens();
             string l_str = lexer.ToString();
